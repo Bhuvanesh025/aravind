b8d1e14ea3ea47d2d4a15244f72e1884e075b3aa
Bhuvanesh025 <ne.bhuvanesh@gmail.com>
2021-03-18 21:32:07 +0530

    one1

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,20 @@
+package org.company;
+
+public class CompanyInfo {
+public void companyName() {
+	System.out.println("Company Name: ABC private limited");
+}
+public void companyId() {
+	System.out.println("Company Id: 001");
+}	
+public void companyAddress() {
+	System.out.println("Company Address: 96,4th floor,JVM towers,Chennai-652369");
+}	
+public static void main(String[] args) {
+	CompanyInfo info = new CompanyInfo();
+	info.companyName();
+	info.companyId();
+	info.companyAddress();
+	
+}		
+}
