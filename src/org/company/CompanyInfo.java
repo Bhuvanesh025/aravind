05fafeab6ac7eb5094ed75806f4c49c4d77cdb42
Bhuvanesh025 <ne.bhuvanesh@gmail.com>
2021-03-18 21:44:57 +0530

    buvi one

--- src/org/company/CompanyInfo.java
+++ src/org/company/CompanyInfo.java
@@ -9,12 +9,17 @@ public void companyId() {
 }	
 public void companyAddress() {
 	System.out.println("Company Address: 96,4th floor,JVM towers,Chennai-652369");
+	
 }	
+public static void aravind() {
+	System.out.println("aravind git");
+}
 public static void main(String[] args) {
 	CompanyInfo info = new CompanyInfo();
 	info.companyName();
 	info.companyId();
 	info.companyAddress();
+	aravind();
 	
 }		
 }
